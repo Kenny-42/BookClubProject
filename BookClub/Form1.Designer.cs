d9ef4cc54f4de9fd897f520efbe935ff7d914d79
Kendall Dawson <lilkenny642@gmail.com>
2025-07-24 20:15:04 -0700

    Refactor main form to Login interface
    Updated Form1 to Login, including UI elements for login and account creation. Modified designer, main class, and program entry point.

--- BookClub/Form1.Designer.cs
+++ BookClub/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BookClub
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            lblLogin = new Label();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            btnLogin = new Button();
+            btnCreateAccount = new Button();
+            SuspendLayout();
+            // 
+            // lblLogin
+            // 
+            lblLogin.AutoSize = true;
+            lblLogin.Font = new Font("Segoe UI", 20F);
+            lblLogin.Location = new Point(336, 9);
+            lblLogin.Name = "lblLogin";
+            lblLogin.Size = new Size(103, 46);
+            lblLogin.TabIndex = 0;
+            lblLogin.Text = "Login";
+            lblLogin.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(252, 88);
+            txtUsername.Name = "txtUsername";
+            txtUsername.PlaceholderText = "Username";
+            txtUsername.Size = new Size(265, 27);
+            txtUsername.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(252, 148);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PlaceholderText = "Password";
+            txtPassword.Size = new Size(265, 27);
+            txtPassword.TabIndex = 2;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(312, 210);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(147, 53);
+            btnLogin.TabIndex = 3;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // btnCreateAccount
+            // 
+            btnCreateAccount.Location = new Point(276, 313);
+            btnCreateAccount.Name = "btnCreateAccount";
+            btnCreateAccount.Size = new Size(228, 50);
+            btnCreateAccount.TabIndex = 4;
+            btnCreateAccount.Text = "Create Account";
+            btnCreateAccount.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnCreateAccount);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Controls.Add(lblLogin);
+            Name = "Login";
+            Text = "Login";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblLogin;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private Button btnLogin;
+        private Button btnCreateAccount;
     }
 }
