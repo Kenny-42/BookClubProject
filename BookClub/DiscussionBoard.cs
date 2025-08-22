028a5e3871c474288d5df6443b198ba4778977ce
Kendall Dawson <lilkenny642@gmail.com>
2025-08-22 13:24:14 -0700

    Add button click event handlers in DiscussionBoard
    Implemented event handlers for btnLogout, btnReviews, and btnBookList to navigate to their respective forms. The btnLogout_Click method opens the Login form, while btnBookList_Click and btnReviews_Click open the BookList and Reviews forms, respectively. Also set the FormClosed event to exit the application when the DiscussionBoard form is closed.

--- BookClub/DiscussionBoard.cs
+++ BookClub/DiscussionBoard.cs
@@ -15,6 +15,28 @@ namespace BookClub
         public DiscussionBoard()
         {
             InitializeComponent();
+            this.FormClosed += (s, args) => Application.Exit();
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            Login loginForm = new Login();
+            loginForm.Show();
+            this.Hide();
+        }
+
+        private void btnBookList_Click(object sender, EventArgs e)
+        {
+            BookList bookListForm = new BookList();
+            bookListForm.Show();
+            this.Hide();
+        }
+
+        private void btnReviews_Click(object sender, EventArgs e)
+        {
+            Reviews reviewsForm = new Reviews();
+            reviewsForm.Show();
+            this.Hide();
         }
     }
 }
