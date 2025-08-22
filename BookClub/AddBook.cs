79322560ac6ed1d1470e9ec0a9a6e93e6cde9390
Kendall Dawson <lilkenny642@gmail.com>
2025-08-22 13:23:57 -0700

    Add button event handlers and form close behavior
    Implemented event handlers for `btnLogout`, `btnBookList`, and `btnSubmit` in `AddBook.Designer.cs`. Defined their click behaviors in `AddBook.cs` to navigate to the respective forms. Added a `FormClosed` event handler to exit the application when the form is closed.

--- BookClub/AddBook.cs
+++ BookClub/AddBook.cs
@@ -15,6 +15,28 @@ namespace BookClub
         public AddBook()
         {
             InitializeComponent();
+            this.FormClosed += (s, args) => Application.Exit();
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            Login loginForm = new Login();
+            loginForm.Show();
+            this.Hide();
+        }
+
+        private void btnBookList_Click(object sender, EventArgs e)
+        {
+            BookList bookListForm = new BookList();
+            bookListForm.Show();
+            this.Hide();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            BookList bookListForm = new BookList();
+            bookListForm.Show();
+            this.Hide();
         }
     }
 }
