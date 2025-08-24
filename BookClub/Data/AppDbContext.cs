eb791d56873e2d37cee8fa14269d71240e81879b
CW-ISM <cwismgitism@gmail.com>
2025-08-24 12:28:21 -0700

    Created AppDbContext and AppDbContextFactory
    - AppDbContext provides access to the database and configures constraints on columns (e.g., unique indexes, check constraints).
    - AppDbContextFactory enables design-time creation of the context for EF Core tools, loading the connection string from appsettings.json.

---
+++ BookClub/Data/AppDbContext.cs
@@ -0,0 +1,57 @@
+﻿using BookClub.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookClub.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Account> Accounts { get; set; }
+    // TODO: Add other tables here as DbSet<T>
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        // Configure the Account entity
+        modelBuilder.Entity<Account>(entity =>
+        {
+            modelBuilder.Entity<Account>()
+                .HasIndex(a => a.Username)
+                .IsUnique();
+
+            modelBuilder.Entity<Account>()
+                .HasIndex(a => a.Email)
+                .IsUnique();
+
+            modelBuilder.Entity<Account>()
+                .Property(a => a.CreatedAt)
+                .HasDefaultValueSql("GETUTCDATE()");
+
+            modelBuilder.Entity<Account>()
+                .ToTable(tb => tb.HasCheckConstraint("CK_Birthdate_AgeRange",
+                "Birthdate <= DATEADD(YEAR, -13, GETDATE()) AND Birthdate >= DATEADD(YEAR, -100, GETDATE())"));
+
+            //entity.HasKey(e => e.AccountId);
+            //entity.Property(e => e.CreatedAt)
+            //      .HasDefaultValueSql("GETDATE()")
+            //      .ValueGeneratedOnAdd();
+            //entity.Property(e => e.FirstName)
+            //      .IsRequired()
+            //      .HasMaxLength(64);
+            //entity.Property(e => e.LastName)
+            //      .IsRequired()
+            //      .HasMaxLength(64);
+            //entity.Property(e => e.Birthdate)
+            //      .IsRequired();
+            //entity.Property(e => e.Email)
+            //      .IsRequired()
+            //      .HasMaxLength(256);
+            //entity.Property(e => e.Username)
+            //      .IsRequired()
+            //      .HasMaxLength(64);
+            //entity.Property(e => e.PasswordHash)
+            //      .IsRequired();
+        });
+    }
+}
