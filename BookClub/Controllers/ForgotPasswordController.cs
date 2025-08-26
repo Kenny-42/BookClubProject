c6aa24653671661eeeba926cbeb658d34d7645e2
CW-ISM <cwismgitism@gmail.com>
2025-08-26 01:22:27 -0700

    Add ForgotPasswordController and remove birthdate check
    Implemented ForgotPasswordController for password recovery, including methods for canceling and retrieving passwords. Updated migration files to remove the birthdate check constraint from the Accounts table, reflecting changes in the model snapshot.

---
+++ BookClub/Controllers/ForgotPasswordController.cs
@@ -0,0 +1,46 @@
+﻿using BookClub.Common;
+using BookClub.Data;
+using BookClub.Views;
+
+namespace BookClub.Controllers;
+
+public class ForgotPasswordController : IController
+{
+    private ForgotPasswordView _view;
+    private AppDbContext _context;
+    public event Action<string, object?>? NavigationRequested;
+    public IView View
+    {
+        get => _view;
+        set => _view = (ForgotPasswordView)value;
+    }
+
+    public ForgotPasswordController(ForgotPasswordView view, AppDbContext context)
+    {
+        _view = view;
+        _context = context;
+    }
+
+    internal void Cancel()
+    {
+        NavigationRequested?.Invoke(LoginView.ViewKey, null);
+    }
+
+    internal Result<string> OnGetPasswordClicked(string usernameEmail)
+    {
+        var account = _context.Accounts.FirstOrDefault(a => a.Username == usernameEmail);
+        if (account == null)
+        {
+            account = _context.Accounts.FirstOrDefault(a => a.Email == usernameEmail);
+        }
+
+        if(account != null)
+        {
+            return Result<string>.Ok(account.Password);
+        }
+        else
+        {
+            return Result<string>.Fail("Account not found.");
+        }
+    }
+}
---
+++ BookClub/Migrations/20250825082110_RemoveBirthdateCheck.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookClub.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveBirthdateCheck : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Birthdate_AgeRange",
+                table: "Accounts");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Birthdate_AgeRange",
+                table: "Accounts",
+                sql: "Birthdate <= DATEADD(YEAR, -13, GETDATE()) AND Birthdate >= DATEADD(YEAR, -100, GETDATE())");
+        }
+    }
+}
