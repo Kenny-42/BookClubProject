3d0592f25ab222207d9555ceb1d4b53eca6bab39
CW-ISM <cwismgitism@gmail.com>
2025-08-26 06:14:12 -0700

    Refactor Login class to use dependency injection
    - Added `using Microsoft.Extensions.DependencyInjection` for DI support.
    - Introduced `AppDbContext` field in `Login` class.
    - Modified constructor to accept `AppDbContext` and initialize it.
    - Updated `btnLogin_Click` to resolve `BookList` via DI and show it.
    - Added `btnCreateAccount_Click` to resolve `CreateAccount` via DI.
    - Removed direct instantiation of `BookList` and `CreateAccount` forms.

--- BookClub/Login.cs
+++ BookClub/Login.cs
@@ -1,29 +1,33 @@
 using BookClub.Data;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace BookClub
+namespace BookClub;
+
+public partial class Login : Form
 {
-    public partial class Login : Form
+    private AppDbContext _context;
+    public Login(AppDbContext context)
     {
-        private AppDbContext _context;
-        public Login(AppDbContext context)
-        {
-            InitializeComponent();
-            _context = context;
-            //this.FormClosed += (s, args) => Application.Exit();
-        }
+        InitializeComponent();
+        _context = context;
+    }
 
-        private void btnLogin_Click(object sender, EventArgs e)
-        {
-            BookList bookListForm = new BookList();
-            bookListForm.Show();
-            this.Hide();
-        }
+    private void btnLogin_Click(object sender, EventArgs e)
+    {
+        // Resolve BookList form via DI
+        var bookListForm = Program.AppServices.GetRequiredService<BookList>();
 
-        private void btnCreateAccount_Click(object sender, EventArgs e)
-        {
-            CreateAccount createAccountForm = new CreateAccount();
-            createAccountForm.Show();
-            this.Hide();
-        }
+        // Show the BookList form
+        bookListForm.Show();
+
+        // Hide the current Login form
+        this.Hide();
+    }
+
+    private void btnCreateAccount_Click(object sender, EventArgs e)
+    {
+        CreateAccount createAccountForm = Program.AppServices.GetRequiredService<CreateAccount>();
+        createAccountForm.Show();
+        this.Hide();
     }
 }
