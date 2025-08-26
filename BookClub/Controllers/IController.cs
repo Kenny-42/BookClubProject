27a89a384a27247bbabb8422fb8bb50783ea6c26
CW-ISM <cwismgitism@gmail.com>
2025-08-26 01:21:19 -0700

    Refactor IController interface
    Removed Initialize, Dispose methods, and NavigationRequested event. Retained View property of type IView.

--- BookClub/Controllers/IController.cs
+++ BookClub/Controllers/IController.cs
@@ -4,9 +4,6 @@ namespace BookClub.Controllers;
 
 public interface IController
 {
-    void Initialize();
-    void Dispose();
-
     event Action<string, object?>? NavigationRequested;
     IView View { get; set; }
 }
