9c06cffc10e77a7fae1fd7826b0910afdf0e4f4e
CW-ISM <cwismgitism@gmail.com>
2025-08-26 05:28:17 -0700

    Add Account class with properties and constructors
    Introduces the `Account` class in the `BookClub.Models` namespace.
    Includes properties for `Id`, `FirstName`, `LastName`, `Username`,
    and `Password`, with appropriate data annotations. Two constructors
    are provided: one for initializing all properties and a parameterless
    constructor.

---
+++ BookClub/Models/Account.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookClub.Models;
+
+public class Account
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+
+    public Account(int id, string firstName, string lastName, string username, string password)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        Username = username;
+        Password = password;
+    }
+    public Account() { }
+}
