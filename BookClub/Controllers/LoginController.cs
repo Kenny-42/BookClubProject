b6a46380464b322f75644d34eca29538d71dddbe
CW-ISM <cwismgitism@gmail.com>
2025-08-25 22:17:04 -0700

    Implement LoginController and LoginView classes
    - Added LoginController to manage login interactions, including event handling for registration and password recovery.
    - Created LoginView class inheriting from UserControl, with event declarations and UI setup in the constructor.
    - Introduced LoginView.resx for resource management and localization of the login interface.

---
+++ BookClub/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using BookClub.Data;
+using BookClub.Views;
+
+namespace BookClub.Controllers;
+
+public class LoginController : IController, IDisposable
+{
+    private LoginView _view;
+    private AppDbContext _context;
+    public event Action<string, object?>? NavigationRequested;
+    public IView View
+    {
+        get => _view;
+        set
+        {
+            _view = (LoginView)value;
+            Initialize();
+        }
+    }
+
+    public LoginController(LoginView view, AppDbContext context)
+    {
+        _view = view;
+        _context = context;
+        Initialize();
+    }
+
+    public void Initialize()
+    {
+        _view.RegisterClicked += OnRegisterClicked;
+        _view.ForgotPasswordClicked += OnForgotPasswordClicked;
+    }
+
+    public void Dispose()
+    {
+        _view.RegisterClicked -= OnRegisterClicked;
+        _view.ForgotPasswordClicked -= OnForgotPasswordClicked;
+    }
+
+    private void OnRegisterClicked(object? sender, EventArgs e)
+    {
+        //await _accountsService.TestDatabaseOperation();
+        NavigationRequested?.Invoke(RegisterView.ViewKey, null);
+    }
+
+    private void OnForgotPasswordClicked(object? sender, EventArgs e) { }
+    private void OnLoginClicked(object? sender, EventArgs e) { }
+}
