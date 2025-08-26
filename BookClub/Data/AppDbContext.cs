ecda9cffd5cafef1e75dedad6362ae928a3e60b6
CW-ISM <cwismgitism@gmail.com>
2025-08-26 06:29:21 -0700

    Add Book entity to AppDbContext with seed data
    Updated AppDbContext to include a DbSet<Book> property.
    Seeded initial data for the Book entity with five entries.

--- BookClub/Data/AppDbContext.cs
+++ BookClub/Data/AppDbContext.cs
@@ -7,6 +7,7 @@ namespace BookClub.Data;
 public class AppDbContext : DbContext
 {
     public DbSet<Account> accounts { get; set; }
+    public DbSet<Book> books { get; set; }
 
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
@@ -19,6 +20,15 @@ public class AppDbContext : DbContext
                 new Account { FirstName = "Charlie", LastName = "Brown", Username = "charlie_brown" },
                 new Account { FirstName = "Diana", LastName = "White", Username = "diana_white" },
                 new Account { FirstName = "Ella", LastName = "Green", Username = "ella_green" }
+            );
+
+        modelBuilder.Entity<Book>()
+            .HasData(
+                new Book { Title = "The Silent Library", Author = "Ava Miles" },
+                new Book { Title = "Gardens of Glass", Author = "Liam Frost" },
+                new Book { Title = "Moonlight Archive", Author = "Elena Brook" },
+                new Book { Title = "Echoes of Dust", Author = "Mason Quinn" },
+                new Book { Title = "Notes from the Attic", Author = "Harper Lee" }
             );
     }
 }
