3b96ae7e4de1508de57bf39c70b2e177ddb4ca90
CW-ISM <cwismgitism@gmail.com>
2025-08-26 06:11:53 -0700

    Refactor AddBook to use dependency injection
    Updated the AddBook class to utilize dependency injection for AppDbContext.
    Modified event handlers to retrieve forms using Program.AppServices.GetRequiredService<T>() instead of direct instantiation.
    Removed unnecessary using directives and the FormClosed event handler from the constructor.

--- BookClub/AddBook.cs
+++ BookClub/AddBook.cs
@@ -1,42 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using BookClub.Data;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace BookClub
+namespace BookClub;
+
+public partial class AddBook : Form
 {
-    public partial class AddBook : Form
+    private AppDbContext _context;
+    public AddBook(AppDbContext context)
     {
-        public AddBook()
-        {
-            InitializeComponent();
-            this.FormClosed += (s, args) => Application.Exit();
-        }
+        InitializeComponent();
+        _context = context;
+    }
 
-        private void btnLogout_Click(object sender, EventArgs e)
-        {
-            Login loginForm = new Login();
-            loginForm.Show();
-            this.Hide();
-        }
+    private void btnLogout_Click(object sender, EventArgs e)
+    {
+        var loginForm = Program.AppServices.GetRequiredService<Login>();
+        loginForm.Show();
+        this.Hide();
+    }
 
-        private void btnBookList_Click(object sender, EventArgs e)
-        {
-            BookList bookListForm = new BookList();
-            bookListForm.Show();
-            this.Hide();
-        }
+    private void btnBookList_Click(object sender, EventArgs e)
+    {
+        BookList bookListForm = Program.AppServices.GetRequiredService<BookList>();
+        bookListForm.Show();
+        this.Hide();
+    }
 
-        private void btnSubmit_Click(object sender, EventArgs e)
-        {
-            BookList bookListForm = new BookList();
-            bookListForm.Show();
-            this.Hide();
-        }
+    private void btnSubmit_Click(object sender, EventArgs e)
+    {
+        BookList bookListForm = Program.AppServices.GetRequiredService<BookList>();
+        bookListForm.Show();
+        this.Hide();
     }
 }
