a6468aa75156aaf821c3c27b65aaa60f64b073b6
CW-ISM <cwismgitism@gmail.com>
2025-08-26 06:13:42 -0700

    Refactor CreateAccount for dependency injection
    Updated the CreateAccount class to utilize dependency injection for the AppDbContext. Modified btnLogin_Click and btnCreateAccount_Click methods to retrieve instances of Login and BookList from the service provider. Cleaned up using directives and removed the FormClosed event handler that called Application.Exit().

--- BookClub/CreateAccount.cs
+++ BookClub/CreateAccount.cs
@@ -1,35 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using BookClub.Data;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace BookClub
+namespace BookClub;
+
+public partial class CreateAccount : Form
 {
-    public partial class CreateAccount : Form
+    private AppDbContext _context;
+    public CreateAccount(AppDbContext context)
     {
-        public CreateAccount()
-        {
-            InitializeComponent();
-            this.FormClosed += (s, args) => Application.Exit();
-        }
+        InitializeComponent();
+        _context = context;
+    }
 
-        private void btnLogin_Click(object sender, EventArgs e)
-        {
-            Login loginForm = new Login();
-            loginForm.Show();
-            this.Hide();
-        }
+    private void btnLogin_Click(object sender, EventArgs e)
+    {
+        Login loginForm = Program.AppServices.GetRequiredService<Login>();
+        loginForm.Show();
+        this.Hide();
+    }
 
-        private void btnCreateAccount_Click(object sender, EventArgs e)
-        {
-            BookList bookListForm = new BookList();
-            bookListForm.Show();
-            this.Hide();
-        }
+    private void btnCreateAccount_Click(object sender, EventArgs e)
+    {
+        BookList bookListForm = Program.AppServices.GetRequiredService<BookList>();
+        bookListForm.Show();
+        this.Hide();
     }
 }
