db8540370a5a421d36345c47cfb5a441d44e5540
CW-ISM <cwismgitism@gmail.com>
2025-08-25 22:19:19 -0700

    Updated `MainForm.cs` to include Login and Register views and controllers. Removed redundant code and comments. Changed initial navigation to start at `LoginView` for improved user experience.

--- BookClub/MainForm.cs
+++ BookClub/MainForm.cs
@@ -1,43 +1,39 @@
 using BookClub.Controllers;
-using BookClub.Data;
-using BookClub.Models;
-using BookClub.Services;
 using BookClub.Views;
-using Microsoft.Extensions.DependencyInjection;
-using System;
 
-namespace BookClub
+namespace BookClub;
+
+public partial class MainForm : Form
 {
-    public partial class MainForm : Form
+    private readonly IServiceProvider _serviceProvider;
+    private readonly ViewNavigator _navigator;
+    private Panel _viewContainer;
+
+    public MainForm(IServiceProvider services)
     {
-        private readonly IServiceProvider _serviceProvider;
-        private readonly ViewNavigator _navigator;
-        private Panel _viewContainer;
-        private AppDbContext _context;
-        public MainForm(IServiceProvider services)
+        InitializeComponent();
+
+        // Setup _viewContainer
+        _viewContainer = new Panel
         {
-            InitializeComponent();
+            Dock = DockStyle.Fill
+        };
+        this.Controls.Add(_viewContainer);
 
-            // Setup _viewContainer
-            _viewContainer = new Panel
-            {
-                Dock = DockStyle.Fill
-            };
-            this.Controls.Add(_viewContainer);
+        // Setup ServiceProvider
+        _serviceProvider = services;
 
-            // Setup ServiceProvider
-            _serviceProvider = services;
+        // Setup ViewNavigator
+        _navigator = new ViewNavigator(_viewContainer, _serviceProvider);
 
-            // Setup ViewNavigator
-            _navigator = new ViewNavigator(_viewContainer, _serviceProvider);
-
-            // Register views
-            _navigator.RegisterView<ViewRed, RedController>(ViewRed.ViewKey);
-            _navigator.RegisterView<ViewBlue, BlueController>(ViewBlue.ViewKey);
+        // Register views
+        _navigator.RegisterView<ViewRed, RedController>(ViewRed.ViewKey);
+        _navigator.RegisterView<ViewBlue, BlueController>(ViewBlue.ViewKey);
+        _navigator.RegisterView<LoginView, LoginController>(LoginView.ViewKey);
+        _navigator.RegisterView<RegisterView, RegisterController>(RegisterView.ViewKey);
 
 
-            // Start app
-            _navigator.NavigateTo(ViewRed.ViewKey);
-        }
+        // Start app
+        _navigator.NavigateTo(LoginView.ViewKey);
     }
 }
