caddf926a5737679a86ba7b41d861a9908a44b9d
CW-ISM <cwismgitism@gmail.com>
2025-08-26 05:28:44 -0700

    Add AppDbContext with DbSet<Account> property
    Introduces the AppDbContext class inheriting from DbContext.
    Includes a DbSet<Account> property for managing accounts.
    Overrides the OnConfiguring method with the base call commented out.

---
+++ BookClub/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookClub.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookClub.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Account> accounts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        //base.OnConfiguring(optionsBuilder);
+    }
+}
