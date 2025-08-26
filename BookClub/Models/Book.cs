88e161c302888ba1e1932767b521f7b733c9a539
CW-ISM <cwismgitism@gmail.com>
2025-08-26 06:29:37 -0700

    Add Book class to BookClub.Models namespace
    Introduces the Book class with properties for Id, Title, and Author. Includes data annotations for key and database generation, along with two constructors for initialization.

---
+++ BookClub/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookClub.Models;
+
+public class Book
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+    public string Author { get; set; }
+
+    public Book(int id, string title, string author)
+    {
+        Id = id;
+        Title = title;
+        Author = author;
+    }
+
+    public Book() { }
+}
