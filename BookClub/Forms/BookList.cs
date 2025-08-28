ad43a2aabca8cf05942b12e7eae30a82b3b24947
Chris Wahl <cwismgitism@gmail.com>
2025-08-27 17:07:10 -0700

    Refactor file directory (#61)
    * Moved all forms into a Forms folder
    
    * Moved `createbookclub.sql` into a Deprecated folder
    
    * Removed forms and sql script from root directory
    
    * Updated namespaces and references to use `BookClub.Forms`

---
+++ BookClub/Forms/BookList.cs
@@ -0,0 +1,83 @@
+﻿using BookClub.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BookClub.Forms;
+
+public partial class BookList : Form
+{
+    private AppDbContext _context;
+    public BookList(AppDbContext context)
+    {
+        InitializeComponent();
+        _context = context;
+        this.FormClosed += (s, args) => Application.Exit();
+        //PopulateBookList();
+    }
+
+    private void btnLogout_Click(object sender, EventArgs e)
+    {
+        Login loginForm = Program.AppServices.GetRequiredService<Login>();
+        loginForm.Show();
+        this.Hide();
+    }
+
+    private void btnAddBook_Click(object sender, EventArgs e)
+    {
+        AddBook addBookForm = Program.AppServices.GetRequiredService<AddBook>();
+        addBookForm.Show();
+        this.Hide();
+    }
+
+    private void PopulateBookList()
+    {
+        pnlBookList.Controls.Clear();
+
+        var books = new List<(int BookId, string Title, string Author)>();
+        string connectionString = "Server=localhost;Database=BookClub;Trusted_Connection=True;TrustServerCertificate=True;";
+        string query = "SELECT BookId, Title, Author FROM Books";
+
+        using (var conn = new SqlConnection(connectionString))
+        using (var cmd = new SqlCommand(query, conn))
+        {
+            conn.Open();
+            using (var reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    books.Add((reader.GetInt32(0), reader.GetString(1), reader.GetString(2)));
+                }
+            }
+        }
+
+        if (books.Count == 0)
+            return;
+
+        int buttonHeight = 50;
+        int spacing = 10;
+
+        for (int i = 0; i < books.Count; i++)
+        {
+            var book = books[i];
+            Button btn = new Button();
+            btn.Text = $"{book.Title}\nby {book.Author}";
+            btn.Width = pnlBookList.ClientSize.Width - 2 * spacing;
+            btn.Height = buttonHeight;
+            btn.Left = spacing;
+            btn.Top = i * (buttonHeight + spacing);
+            btn.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            btn.Tag = book.BookId;
+            btn.UseCompatibleTextRendering = true;
+
+            btn.Click += (s, e) =>
+            {
+                int bookId = (int)((Button)s).Tag;
+                Reviews reviewsForm = new Reviews(_context, bookId);
+                reviewsForm.Show();
+                this.Hide();
+            };
+
+            pnlBookList.Controls.Add(btn);
+        }
+    }
+}
