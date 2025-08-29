5590f066891944ab64a141f7978628d5551df520
CW-ISM <cwismgitism@gmail.com>
2025-08-28 22:23:47 -0700

    Improve documentation and simplify Book class
    Updated documentation comments in the `Book` class for clarity. Removed the constructor to allow reliance on a parameterless constructor. Added a remark in the `BookUpdateDTO` class to indicate that all properties are optional for partial updates.

--- BookClub/Models/Book.cs
+++ BookClub/Models/Book.cs
@@ -9,46 +9,38 @@ namespace BookClub.Models;
 public class Book
 {
     /// <summary>
-    /// This is the unique identifier for the book.
+    /// Gets or sets the unique identifier for the entity.
     /// </summary>
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
 
     /// <summary>
-    /// This is the title of the book.
+    /// Gets or sets the title of the entity. This value is required.
     /// </summary>
     [Required]
     public string Title { get; set; }
 
     /// <summary>
-    /// This is the author of the book.
+    /// Gets or sets the name of the author.
     /// </summary>
     [Required]
     public string Author { get; set; }
 
     /// <summary>
-    /// This is a brief description of the book.
+    /// Gets or sets the description text.
     /// </summary>
     [StringLength(1024, ErrorMessage = "Description cannot be longer than 1024 characters.")]
     public string Description { get; set; }
 
 
     /// <summary>
-    /// This is a simplified ISBN validation.
+    /// Gets or sets the International Standard Book Number (ISBN) of the book.
     /// </summary>
     [Required]
     [StringLength(13, ErrorMessage = "ISBN must be 13 characters long.")]
     public string ISBN { get; set; }
 
-    /// <summary>
-    /// This is the constructor for the Book class.
-    /// </summary>
-    /// <param name="id"></param>
-    /// <param name="title"></param>
-    /// <param name="author"></param>
-    /// <param name="description"></param>
-    /// <param name="iSBN"></param>
     public Book(int id, string title, string author, string description, string iSBN)
     {
         Id = id;
@@ -58,16 +50,13 @@ public class Book
         ISBN = iSBN;
     }
 
-    /// <summary>
-    /// This is a parameterless constructor for EF Core.
-    /// </summary>
     public Book() { }
 }
 
 /// <summary>
 /// This is a Data Transfer Object (DTO) for updating Book entities.
-/// Uses nullable properties to allow partial updates.
 /// </summary>
+/// <remarks>All properties are optional to allow partial updates.</remarks>
 public class BookUpdateDTO
 {
     public string? Title { get; set; }
