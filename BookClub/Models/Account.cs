12213cbb96ac79b20fb143943826a4a3c81a0d91
CW-ISM <cwismgitism@gmail.com>
2025-08-28 22:16:50 -0700

    Add XML documentation and AccountUpdateDTO class
    Enhanced `Account.cs` with XML documentation for the `Account` class and its properties to improve code readability. Introduced `AccountUpdateDTO` class with optional properties for partial updates to account entities.

--- BookClub/Models/Account.cs
+++ BookClub/Models/Account.cs
@@ -3,24 +3,45 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BookClub.Models;
 
+/// <summary>
+/// This is the Account entity model.
+/// </summary>
 public class Account
 {
+    /// <summary>
+    /// Gets or sets the unique identifier for the entity.
+    /// </summary>
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; } = 0; // EF Core will auto-generate the ID - Initialize to 0 to avoid uninitialized warning
     
+    /// <summary>
+    /// Gets or sets the first name of the individual.
+    /// </summary>
     [Required]
     public string FirstName { get; set; }
 
+    /// <summary>
+    /// Gets or sets the last name of the individual.
+    /// </summary>
     [Required]
     public string LastName { get; set; }
 
+    /// <summary>
+    /// Gets or sets the email address associated with the entity.
+    /// </summary>
     [Required]
     public string Email { get; set; }
 
+    /// <summary>
+    /// Gets or sets the username associated with the user.
+    /// </summary>
     [Required]
     public string Username { get; set; }
 
+    /// <summary>
+    /// Gets or sets the password associated with the user.
+    /// </summary>
     [Required]
     public string Password { get; set; }
 
@@ -39,6 +60,7 @@ public class Account
 /// <summary>
 /// This is a Data Transfer Object (DTO) for updating Account entities.
 /// </summary>
+/// <remarks>All properties are optional to allow partial updates.</remarks>
 public class AccountUpdateDTO
 {
     public string? FirstName { get; set; }
