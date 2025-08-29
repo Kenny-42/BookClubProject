36b188398518b39505d4c14ded40b31bac3652ba
Kendall Dawson <lilkenny642@gmail.com>
2025-08-29 02:16:33 -0700

    Add star rating system to EditReview class
    This commit introduces a `StarSystemController` to manage a star rating system in the `EditReview` class. A new using directive for `BookClub.Handlers` is added, and the `starController` is initialized in the constructor, linking it to five `PictureBox` controls. Event handling for rating changes is set up to update the `currentRating` variable.

--- BookClub/Forms/EditReview.cs
+++ BookClub/Forms/EditReview.cs
@@ -7,14 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BookClub.Handlers;
 
 namespace BookClub.Forms
 {
     public partial class EditReview : Form
     {
+        private StarSystemController starController;
+        private int currentRating = 0;
+
         public EditReview()
         {
             InitializeComponent();
+            // Initialize the star rating system using StarSystemController
+            starController = new StarSystemController(
+                new PictureBox[] { pcbStar1, pcbStar2, pcbStar3, pcbStar4, pcbStar5 },
+                Properties.Resources.star_filled,
+                Properties.Resources.star_empty
+            );
+            starController.RatingChanged += (rating) => { currentRating = rating; };
         }
     }
 }
