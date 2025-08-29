bae4dfe994a91b46c33d9f63ae5f5d56d5cebea1
Chris Wahl <cwismgitism@gmail.com>
2025-08-28 19:21:31 -0700

    Rename tables and update migrations for consistency (#115)
    Updated Entity Framework Core migration files and model snapshot in the `BookClub` application. Renamed database tables from lowercase (`books` and `reviews`) to PascalCase (`Books` and `Reviews`). Added primary keys for the renamed tables and updated the `BuildTargetModel` method to reflect the new table names, including seed data for `Account`, `Book`, and `Review` entities.

---
+++ BookClub/Migrations/20250829022015_ModelChangeErrorFix.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookClub.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModelChangeErrorFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_reviews",
+                table: "reviews");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_books",
+                table: "books");
+
+            migrationBuilder.RenameTable(
+                name: "reviews",
+                newName: "Reviews");
+
+            migrationBuilder.RenameTable(
+                name: "books",
+                newName: "Books");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Reviews",
+                table: "Reviews",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Books",
+                table: "Books",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Reviews",
+                table: "Reviews");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Books",
+                table: "Books");
+
+            migrationBuilder.RenameTable(
+                name: "Reviews",
+                newName: "reviews");
+
+            migrationBuilder.RenameTable(
+                name: "Books",
+                newName: "books");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_reviews",
+                table: "reviews",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_books",
+                table: "books",
+                column: "Id");
+        }
+    }
+}
